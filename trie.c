f1ae63f2522746c0e9d7b754753b19eb44771b70
Seyed Mahdi Nagavi Kalajahi <44202187+smahdink@users.noreply.github.com>
2019-02-21 22:30:58 +0330

    Add files via upload

---
+++ trie.c
@@ -0,0 +1,141 @@
+// In God we trust
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef struct node
+{
+    char name[40];
+    struct node *next[10];
+}
+node;
+
+node *append(node *ptr)
+{
+    // Assigns a new node to a pointer
+    node *new = malloc(sizeof(node));
+    for (int i = 0; i < 10; i++)
+    {
+        new->next[i] = NULL;
+    }
+
+    // Checks whether the given pointer is NULL or not
+    if (ptr == NULL)
+    {
+        ptr = new;
+        return ptr;
+    }
+    else
+    {
+        return ptr;
+    }
+}
+
+int main(void)
+{
+    char word[40];
+    char date[5];
+    node *trie = NULL;
+    trie = append(trie);
+    node *tmp = trie;
+
+    printf("Trie V 0.01\n");
+    printf("Please enter the name of person and year of birth in four digits(type \"end\" to finish input)\n");
+
+    while (1)
+    {
+        printf("%i\n", sizeof(node));
+        printf("Word: ");
+        scanf("%s", word);
+        if (strcmp(word, "end") == 0)
+        {
+            break;
+        }
+
+        while (1)
+        {
+            // Gets and handles the errors with it
+            printf("Date: ");
+            scanf("%s", date);
+            if (strcmp(date, "end") == 0)
+            {
+                break;
+            }
+
+            if (!isdigit(date[0]) && !isdigit(date[1]) && !isdigit(date[2]) && !isdigit(date[3]))
+            {
+                printf("Please enter a number!\n");
+                continue;
+            }
+            else
+            {
+                break;
+            }
+            
+        }
+        if (strcmp(date, "end") == 0)
+        {
+            break;
+        }
+        
+        tmp = trie;
+        for (int i = 0; i < 4; i++)
+        {
+            // Adds the name to trie
+            int place = date[i] - 48;
+            tmp->next[place] = append(tmp->next[place]);
+            tmp = tmp->next[place];
+        }
+        strcpy(tmp->name, word);
+    }
+
+    printf("Now enter the Date to search(type \"end\" to finish search)\n");
+
+    while (1)
+    {
+        char date[5];
+        while (1)
+        {
+            // Gets and handles the errors with an int
+            printf("Date: ");
+            scanf("%s", date);
+            if (strcmp(date, "end") == 0)
+            {
+                break;
+            }
+
+            if (!isdigit(date[0]) && !isdigit(date[1]) && !isdigit(date[2]) && !isdigit(date[3]))
+            {
+                printf("Please enter a number!\n");
+                continue;
+            }
+            else
+            {
+                break;
+            }
+            
+        }
+        if (strcmp(date, "end") == 0)
+        {
+            break;
+        }
+
+        tmp = trie;
+        char result[40];
+        for (int i = 0; i < 4; i++)
+        {
+            int place = date[i] - 48;
+            if (tmp->next[place] == NULL)
+            {
+                strcpy(result, "Not found!");
+                break;
+            }
+            tmp = tmp->next[place];
+        }
+        
+        printf("Result: %s\n", result);
+
+    }
+
+}
